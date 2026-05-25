dee6a8a8b560a673a27dad9bfbe025ec276699cb
avr_dude <creed_truman77@my.albemarle.edu>
2026-05-24 21:45:48 -0400

    I-type instructions! Hurrah!

--- main.cpp
+++ main.cpp
@@ -23,18 +23,46 @@ bool execute_instruction(uint32_t* regs, uint8_t* mem, uint32_t* pc) {
     printf("opcode: 0x%02X\n", opcode);
     printf("funct7: 0x%02X\n", funct7);
     printf("funct3: 0x%02X\n", funct3);
-    printf("rs1   : 0x%02X\n", rs1   );
-    printf("rs2   : 0x%02X\n", rs2   );
-    printf("rd    : 0x%02X\n", rd    );
+    printf("rs1   : 0x%02X, val: %d\n", rs1   , regs[rs1]);
+    printf("rs2   : 0x%02X, val: %d\n", rs2   , regs[rs2]);
+    printf("rd    : 0x%02X, val: %d\n", rd    , regs[rd ]);
     switch(opcode){
-        case 0x33: // R-type
+        case(0x33):{ // R-type
             printf("R-type instruction\n");
+                 if(funct3 == 0x00 && funct7 == 0x00) regs[rd] = regs[rs1] + regs[rs2];
+            else if(funct3 == 0x00 && funct7 == 0x20) regs[rd] = regs[rs1] - regs[rs2];
+            else if(funct3 == 0x01 && funct7 == 0x00) regs[rd] = regs[rs1] << (regs[rs2] & 0x0000001F);
+            else if(funct3 == 0x02 && funct7 == 0x00) regs[rd] = ((int32_t)regs[rs1]) < ((int32_t)regs[rs2]);
+            else if(funct3 == 0x03 && funct7 == 0x00) regs[rd] = regs[rs1] < regs[rs2];
+            else if(funct3 == 0x04 && funct7 == 0x00) regs[rd] = regs[rs1] ^ regs[rs2];
+            else if(funct3 == 0x05 && funct7 == 0x00) regs[rd] = regs[rs1] >> (regs[rs2] & 0x0000001F);
+            else if(funct3 == 0x05 && funct7 == 0x20) regs[rd] = ((int32_t)regs[rs1]) >> (regs[rs2] & 0x0000001F);
+            else if(funct3 == 0x06 && funct7 == 0x00) regs[rd] = regs[rs1] | regs[rs2];
+            else if(funct3 == 0x07 && funct7 == 0x00) regs[rd] = regs[rs1] & regs[rs2];
+            else printf("Undefined R-Type Instructions!!!\n\n");
             break;
+        }
+        case(0x13):{ // I-type
+            printf("I-type instruction\n");
+                int32_t seimm = ((int32_t)instruction) >> 20;
+                uint32_t uimm  = seimm;
+                 if(funct3 == 0x00                  ) regs[rd] = regs[rs1] + seimm;
+            else if(funct3 == 0x01 && funct7 == 0x00) regs[rd] = regs[rs1] << (uimm & 0x0000001F);
+            else if(funct3 == 0x02                  ) regs[rd] = ((int32_t)regs[rs1]) < seimm;
+            else if(funct3 == 0x03                  ) regs[rd] = regs[rs1] < uimm;
+            else if(funct3 == 0x04                  ) regs[rd] = regs[rs1] ^ seimm;
+            else if(funct3 == 0x05 && funct7 == 0x00) regs[rd] = regs[rs1] >> (uimm & 0x0000001F);
+            else if(funct3 == 0x05 && funct7 == 0x20) regs[rd] = ((int32_t)regs[rs1]) >> (uimm & 0x0000001F);
+            else if(funct3 == 0x06                  ) regs[rd] = regs[rs1] | seimm;
+            else if(funct3 == 0x07                  ) regs[rd] = regs[rs1] & seimm;
+            else printf("Undefined I-Type Instructions!!!\n\n");
+            break;
+        }
         default:
             printf("Unknown instruction opcode!!!");
             return false; // Invalid instruction
     }
-
+    printf("New rd value: %d\n\n", regs[rd]);
     *pc += 4;
     if(*pc >= MEM_SIZE){
         printf("pc (0x%08X) has exceeded memsize (0x%08X)!", *pc, MEM_SIZE);
@@ -50,7 +78,8 @@ int main(){
     uint32_t regs[32] = {0}; // 32 registers initialized to 0
     uint8_t mem[MEM_SIZE] = {0}; // 4KB of memory initialized to 0
     uint32_t pc = 0; // Program counter initialized to 0
-
+    regs[1] = 16;//For arithmetic testing until I have immediate or memory loading commands
+    regs[2] = 2;
     FILE* testing_binary = fopen("rv_test.bin", "rb");
     if (testing_binary == NULL) {
         printf("Error: Could not open rv_test.bin!\n");
